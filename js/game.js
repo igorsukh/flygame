0f47cc1fd0dcae0b970969f053aed853e5e72e6b
Igor <igorsukh@gmail.com>
2020-01-18 12:41:54 +0300

    fly game practice

---
+++ js/game.js
@@ -0,0 +1,89 @@
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
+
+let bird = new Image();
+let bg = new Image();
+let fg = new Image();
+let pipeUp = new Image();
+let pipeBottom = new Image();
+
+bird.src = "image/bird.png";
+bg.src = "image/bg.png";
+fg.src = "image/fg.png";
+pipeUp.src = "image/pipeUp.png";
+pipeBottom.src = "image/pipeBottom.png";
+
+//Звуковые файлы
+
+let fly = new Audio();
+let score_audio = new Audio();
+
+fly.src = "audio/fly.mp3";
+score_audio.src = "audio/score.mp3";
+
+let gap = 110;
+
+//При нажатии на какую-либо кнопку
+document.addEventListener("keydown", moveUp);
+
+function moveUp() {
+    yPos -=30; //подпрыгивания птички
+    fly.play();
+}
+
+//Создание блоков
+let pipe = [];
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+}
+
+//Позиция птички
+let xPos = 10;
+let yPos = 150;
+let grav = 1;
+
+let score = 0;
+
+function draw() {
+    ctx.drawImage(bg, 0, 0);
+
+    for (let i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+        pipe[i].x--;
+
+        if (pipe[i].x == 80){
+            pipe.push({
+                x: cvs.width,
+                y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+        }
+
+        if (xPos + bird.width >= pipe[i].x 
+            && xPos <= pipe[i].x + pipeUp.width
+            && (yPos <= pipe[i].y + pipeUp.height
+                || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos +  bird.height >= cvs.height - fg.height) {
+                    location.reload(); //перезапуск игры
+                }
+
+            if (pipe[i].x == 5) {
+                score++;
+                score_audio.play();
+            }
+    }
+
+    ctx.drawImage(fg, 0, cvs.height - fg.height);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+
+    ctx.fillStyle = "#000";
+    ctx.font = "24px Verdana";
+    ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+
+    requestAnimationFrame(draw);
+}
+
+pipeBottom.onload = draw;
